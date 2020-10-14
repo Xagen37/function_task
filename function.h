15947646b7d5c62f8e308ea5292632efdaf3fc8e
Xagen37 <xagen.rus@gmail.com>
2020-10-14 17:15:57 +0300

    Added function realisation

--- function.h
+++ function.h
@@ -1,31 +1,125 @@
 #pragma once
+#include "traits.h"
+#include <memory>
+
 
 template <typename F>
 struct function;
 
-template <typename R, typename... Args>
-struct function<R (Args...)>
+template <typename Return_t, typename... Args>
+struct function<Return_t (Args...)>
 {
-    function() noexcept;
+	using storage_t = storage<Return_t(Args...)>;
 
-    function(function const& other);
-    function(function&& other) noexcept;
+	function() = default;
+
+	function(const function& other)
+	{
+		if (!other)
+		{
+			stg = std::unique_ptr<storage_t>(nullptr);
+		}
+		else
+		{
+			if (!stg.get())
+			{
+				stg = std::unique_ptr<storage_t>(new storage_t());
+			}
+			other.stg->get_desc()->copy(other.stg.get(), stg.get());
+		}
+	}
+
+	function(function&& other) noexcept
+	{
+		if (!other)
+		{
+			stg = std::unique_ptr<storage_t>(nullptr);
+		}
+		else
+		{
+			if (!stg.get())
+			{
+				stg = std::unique_ptr<storage_t>(new storage_t());
+			}
+			other.stg->get_desc()->move(other.stg.get(), stg.get());
+		}
+	}
+	
+    template <typename T>
+	function(T val)
+		: stg(std::make_unique <storage_t> (std::move(val)))
+	{}
+
+	function& operator=(function const& rhs)
+	{
+		if (this != &rhs)
+		{
+			if (!rhs)
+			{
+				stg.reset();
+			}
+			else
+			{
+				if (!stg.get())
+				{
+					stg = std::unique_ptr<storage_t>(new storage_t());
+				}
+				rhs.stg->get_desc()->copy(rhs.stg.get(), stg.get());
+			}
+		}
+		
+		return *this;
+	}
+	function& operator=(function&& rhs) noexcept
+	{
+		stg.swap(rhs.stg);
+
+		return *this;
+	}
+
+	explicit operator bool() const noexcept
+	{
+		return stg.get();
+	}
+
+	Return_t operator()(Args... args) const
+	{
+		if (!stg.get())
+		{
+			throw bad_function_call();
+		}
+		else
+		{
+			return stg->invoke(std::forward<Args>(args)...);
+		}
+	}
 
     template <typename T>
-    function(T val);
-
-    function& operator=(function const& rhs);
-    function& operator=(function&& rhs) noexcept;
-
-    ~function();
-
-    explicit operator bool() const noexcept;
-
-    R operator()(Args... args) const;
-
-    template <typename T>
-    T* target() noexcept;
+	T* target() noexcept
+	{
+		if (*this)
+		{
+			return stg->template get_dynamic<T>();
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
 
     template <typename T>
-    T const* target() const noexcept;
+	T const* target() const noexcept
+	{
+		if (*this)
+		{
+			return stg->template get_dynamic<T>();
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+
+private:
+	std::unique_ptr<storage_t> stg;
 };
---
+++ traits.h
@@ -0,0 +1,241 @@
+#include <type_traits>
+#include <cstddef>
+#include <utility>
+#include <exception>
+
+constexpr static size_t INPLACE_BUFFER_SIZE = sizeof(void*);
+constexpr static size_t INPLACE_BUFFER_ALIGNMENT = alignof(void*);
+
+using inplace_buffer = std::aligned_storage<
+	INPLACE_BUFFER_SIZE,
+	INPLACE_BUFFER_ALIGNMENT>::type;
+
+template <typename T>
+constexpr static bool fits_small_storage = 
+	sizeof(T) <= INPLACE_BUFFER_SIZE &&
+	INPLACE_BUFFER_ALIGNMENT % alignof(T) == 0 &&
+	std::is_nothrow_move_constructible<T>::value;
+
+struct bad_function_call : std::exception
+{
+	const char* what() const noexcept override
+	{
+		return "bad function call";
+	}
+};
+
+template <typename Sign>
+struct type_descriptor;
+
+template <typename T, typename Sign, typename = void>
+struct function_traits;
+
+template <typename Sign>
+struct storage;
+
+template <typename Return_t, typename... Args>
+struct storage<Return_t(Args...)>
+{
+	storage()
+	{}
+
+	template<typename T>
+	storage(T&& val)
+	{
+		if constexpr (fits_small_storage<T>)
+		{
+			new(&buffer) std::remove_reference_t<T>(std::forward<T>(val));
+		}
+		else
+		{
+			reinterpret_cast<T*&>(buffer) = new std::remove_reference_t<T>(std::forward<T>(val));
+		}
+		set_desc(function_traits<T, Return_t (Args...)>::get_type_descriptor());
+	}
+
+	~storage()
+	{
+		descriptor->destroy(this);
+	}
+
+	template <typename T>
+	T& get_static() noexcept
+	{
+		if constexpr (fits_small_storage<T>)
+		{
+			return reinterpret_cast<T&>(buffer);
+		}
+		else
+		{
+			return *(reinterpret_cast<T* const &>(buffer));
+		}
+	}
+
+	template <typename T>
+	const T& get_static() const noexcept
+	{
+		if constexpr (fits_small_storage<T>)
+		{
+			return reinterpret_cast<const T&>(buffer);
+		}
+		else
+		{
+			return *(reinterpret_cast<const T* const &>(buffer));
+		}
+	}
+
+	void set_dynamic(void* value) noexcept
+	{
+		reinterpret_cast<void*&>(buffer) = value;
+	}
+
+	template <typename T>
+	T* get_dynamic() noexcept
+	{
+		return const_cast<T*>(static_cast <const storage*>(this)->template get_dynamic<T>());
+	}
+
+	template <typename T>
+	const T* get_dynamic() const noexcept
+	{
+		if (function_traits<T, Return_t (Args...)>::get_type_descriptor() == descriptor)
+		{
+			return &(get_static<T>());
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+
+	Return_t invoke(Args&&... args)
+	{
+		return descriptor->invoke(this, std::forward<Args>(args)...);
+	}
+
+	const inplace_buffer& get_buffer() const
+	{
+		return buffer;
+	}
+
+	inplace_buffer& get_buffer()
+	{
+		return buffer;
+	}
+
+	const type_descriptor<Return_t (Args...)>* get_desc() const
+	{
+		return descriptor;
+	}
+
+	void set_desc(const type_descriptor<Return_t(Args...)>* other_desc)
+	{
+		descriptor = other_desc;
+	}
+
+private:
+	inplace_buffer buffer;
+	const type_descriptor<Return_t (Args...)>* descriptor;
+};
+
+template <typename Return_t, typename... Args>
+struct type_descriptor <Return_t(Args...)>
+{
+	using storage_t = storage<Return_t (Args...)>;
+
+	void (*copy)(const storage_t* src, storage_t* dest);
+	void (*move)(storage_t* src, storage_t* dest);
+	Return_t (*invoke)(storage_t* src, Args...);
+	void (*destroy)(storage_t*);
+};
+
+template <typename T, typename Return_t, typename... Args>
+struct function_traits <T, Return_t(Args...), std::enable_if_t<fits_small_storage<T>>>
+{
+	static const type_descriptor <Return_t (Args...)>* get_type_descriptor() noexcept
+	{
+		using storage_t = storage <Return_t (Args...)>;
+
+		constexpr static type_descriptor <Return_t (Args...)> impl =
+		{
+			// Copy
+			[](const storage_t* src, storage_t* dest) 
+			{
+				auto& f = src->template get_static<T>();
+				new(&(dest->get_buffer())) T(f);
+				dest->set_desc(src->get_desc());
+			},
+
+			// move
+			[](storage_t* src, storage_t* dest)
+			{
+				new(&(dest->get_buffer())) T(std::move(src->template get_static<T>()));
+				dest->set_desc(src->get_desc());
+			},
+
+			// invoke
+			[](storage_t* src, Args... args) -> Return_t
+			{
+				return src->template get_static<T>()(std::forward<Args>(args)...);
+			},
+			
+			// destroy
+			[](storage_t* src)
+			{
+				src->template get_static<T>().~T();
+			}
+		};
+
+		return &impl;
+	}
+};
+
+template <typename T, typename Return_t, typename... Args>
+struct function_traits<T, Return_t(Args...), std::enable_if_t<!fits_small_storage<T>>>
+{
+	static void initialize_storage(storage <Return_t (Args...)>& src, T&& obj)
+	{
+		src.set_dynamic(new T(std::move(obj)));
+	}
+
+	static void initialize_storage(storage <Return_t(Args...)>& src, const T& obj)
+	{
+		src.set_dynamic(new T(obj));
+	}
+
+	static const type_descriptor <Return_t (Args...)>* get_type_descriptor() noexcept
+	{
+		using storage_t = storage <Return_t (Args...)>;
+
+		constexpr static type_descriptor <Return_t (Args...)> impl =
+		{
+			// Copy
+			[](const storage_t* src, storage_t* dest) 
+			{
+				initialize_storage(*dest, src->template get_static<T>());
+				dest->set_desc(src->get_desc());
+			},
+
+			// move
+			[](storage_t* src, storage_t* dest) 
+			{
+				initialize_storage(*dest, src->template get_static<T>());
+				dest->set_desc(src->get_desc());
+			},
+
+			// invoke
+			[](storage_t* src, Args... args) 
+			{
+				return src->template get_static<T>()(std::forward<Args>(args)...);
+			},
+
+			// destroy
+			[](storage_t* src) 
+			{
+				src->template get_static<T>().~T();
+			}
+		};
+
+		return &impl;
+	}
+};
